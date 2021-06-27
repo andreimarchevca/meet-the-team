61e7f1f590603f4edea60ad9dafce8801c83e1c4
Andrei Marchevca <amarchevca@complylog.com>
2021-06-27 15:49:57 +0200

    fix small test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders tretton text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/tretton37/i);
   expect(linkElement).toBeInTheDocument();
 });
